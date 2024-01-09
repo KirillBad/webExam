fbb0924e602f03f6dfe2ec4082b3dfd4e749c203
KirillBad <37250878+KirillBad@users.noreply.github.com>
2024-01-10 00:01:58 +0300

    remove api key

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const apiKey = '3c7a9230-b3c9-4927-99d1-c9180f2d30c8';
+const apiKey = '';
 async function getRoutsData() {
     const apiUrl = 'http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes';
     const urlWithApiKey = `${apiUrl}?api_key=${apiKey}`;
