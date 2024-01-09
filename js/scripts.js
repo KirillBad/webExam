503dab241bc9e31578e8266a660c70aafd154429
KirillBad <kirillbad2004@yandex.ru>
2024-01-09 03:30:13 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -523,6 +523,7 @@ async function mainAccount () {
     let inputPeopleCount = document.getElementById('peopleCount');
     let inputTourGuideCheckBox = document.getElementById('tourGuideCheckBox');
     let inputCarCheckBox = document.getElementById('carCheckBox');
+    let idForDelete;
 
     function displayList(arrData, routsArrData, rowsPerPage, page) {
         const tableEl = document.getElementById('tableOrders');
@@ -610,7 +611,7 @@ async function mainAccount () {
             deletebutton.setAttribute('data-bs-target', '#deleteModal');
 
             document.getElementById("deleteConfirmBtn").addEventListener("click", async () => {
-                const idForDelete = paginatedData[key]["id"];
+                idForDelete = paginatedData[key]["id"];
                 await deleteOrder(idForDelete);
                 mainAccount();
             });
